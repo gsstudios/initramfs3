b99b70b995da6c4060fbd61723278c764fbe5aa3
Josh <gsstudios@users.noreply.github.com>
2016-02-16 17:47:52 +1100

    Revert "Sync LP kernel updates"

--- sbin/ext/post-init.sh
+++ sbin/ext/post-init.sh
@@ -79,8 +79,8 @@ if [ -f /data/.siyah/restore_running ]; then
 fi;
 
 # reset profiles auto trigger to be used by kernel ADMIN, in case of need, if new value added in default profiles
-# just set number $RESET_MAGIC + 1 and profiles will be reset one time on next boot with new kernel.
-RESET_MAGIC=9;
+# just set numer $RESET_MAGIC + 1 and profiles will be reset one time on next boot with new kernel.
+RESET_MAGIC=8;
 if [ ! -e /data/.siyah/reset_profiles ]; then
 	echo "0" > /data/.siyah/reset_profiles;
 fi;
