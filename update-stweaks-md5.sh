5a90719bfa9bcaccaf7eb9dcf695befe2954c83f
gsstudios <josh.lay@exemail.com.au>
2016-06-05 16:54:20 +1000

    Restore update Stweaks MD5 script

--- update-stweaks-md5.sh
+++ update-stweaks-md5.sh
@@ -1,3 +1,4 @@
-md5sum /res/misc/payload/STweaks.apk | awk '{print $1}' > /res/stweaks_md5;
-echo "Done!";
-cat /res/stweaks_md5;
+#!/bin/bash
+md5sum res/misc/payload/STweaks.apk | awk '{print $1}' > res/stweaks_md5;
+chmod 644 res/stweaks_md5;
+cat res/stweaks_md5;
