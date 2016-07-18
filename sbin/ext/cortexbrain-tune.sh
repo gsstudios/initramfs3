e6d6051976559ebb033699020205d3100d962034
gsstudios <josh.lay@exemail.com.au>
2016-07-18 17:50:58 +1000

    Add PE workqueue control

--- sbin/ext/cortexbrain-tune.sh
+++ sbin/ext/cortexbrain-tune.sh
@@ -407,6 +407,22 @@ TWEAK_HOTPLUG_ECO()
 	log -p i -t "$FILE_NAME" "*** TWEAK_HOTPLUG_ECO: $state ***";
 }
 
+WORKQUEUE_CONTROL()
+{
+	local state="$1";
+
+	if [ "$state" == "awake" ]; then
+		if [ "$power_efficient" == "on" ]; then
+			echo "Y" > /sys/module/workqueue/parameters/power_efficient;
+		else
+			echo "N" > /sys/module/workqueue/parameters/power_efficient;
+		fi;
+	elif [ "$state" == "sleep" ]; then
+		echo "Y" > /sys/module/workqueue/parameters/power_efficient;
+	fi;
+	log -p i -t "$FILE_NAME" "*** WORKQUEUE_CONTROL ***: done";
+}
+
 CPU_GOV_TWEAKS()
 {
 	local state="$1";
@@ -1533,6 +1549,7 @@ AWAKE_MODE()
 			MOBILE_DATA "awake";
 			WIFI "awake";
 			IO_SCHEDULER "awake";
+			WORKQUEUE_CONTROL "awake";
 			GESTURES "awake";
 			MOUNT_SD_CARD;
 
@@ -1608,6 +1625,7 @@ SLEEP_MODE()
 			CENTRAL_CPU_FREQ "sleep_freq";
 			CPU_GOV_TWEAKS "sleep";
 			IO_SCHEDULER "sleep";
+			WORKQUEUE_CONTROL "sleep";
 			BUS_THRESHOLD "sleep";
 #			KERNEL_SCHED "sleep";
 			NET "sleep";
